1622b679686400543b2f38bc9ccd07f05378505b
Palafox003 <israelpalafox@utgz.edu.mx>
2018-04-16 21:27:56 -0500

    Cambio de Título
    Se cambio el título por uno  mejor

--- Gallery/index.php
+++ Gallery/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Gallery</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/gallery.css">
 </head>
